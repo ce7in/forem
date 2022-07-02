af638eb7d81a7d2143d03de3541be92126f5ba0f
Muhammed Cetin <cetin.muhammed@gmail.com>
2022-07-03 01:01:54 +0300

    Alternative tagged with issues have been fixed.

--- app/controllers/stories/tagged_articles_controller.rb
+++ app/controllers/stories/tagged_articles_controller.rb
@@ -55,7 +55,7 @@ module Stories
     end
 
     def tagged_count(tag:)
-      Article.published.cached_tagged_with(tag).where(score: Settings::UserExperience.tag_feed_minimum_score..).count
+      Article.published.cached_tagged_with(tag).count
       # tag.articles.published.where(score: Settings::UserExperience.tag_feed_minimum_score..).count
     end
 
